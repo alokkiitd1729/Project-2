9d6ae34bdc83ea8ae368fd436e1284ab4ec7ce3c
Alok Kumar <36418056+alokkiitd1729@users.noreply.github.com>
2019-01-26 04:07:11 +0530

    Update WordEntry.java

--- WordEntry.java
+++ WordEntry.java
@@ -1,3 +1,15 @@
+
+
+/**
+ * Project completion Date 28 October 2018,
+ *    Author@_Alok_Kumar
+ *    Indian Institute of Technology(IIT) Delhi
+ *
+ *    To Understand the project in enough detail please go to ReadMe file of Project 2
+ *     and Visit under the section under the name of this class.
+ */
+
+
 public class WordEntry {
  MyLinkedList<Position> ll;
  AVLTree avltree = new AVLTree();
