9ade19ee2cbfc61314957a2f8f5a4107c457de4f
Alok Kumar <36418056+alokkiitd1729@users.noreply.github.com>
2019-01-26 04:06:28 +0530

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,3 +1,15 @@
+
+
+/**
+ * Project completion Date 28 October 2018,
+ *    Author@_Alok_Kumar
+ *    Indian Institute of Technology(IIT) Delhi
+ *
+ *    To Understand the project in enough detail please go to ReadMe file of Project 2
+ *     and Visit under the section under the name of this class.
+ */
+
+
 import java.io.FileInputStream;
 import java.util.Scanner;
 import java.util.Vector;
